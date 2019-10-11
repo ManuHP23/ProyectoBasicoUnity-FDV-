7a3afa347da327c15a4661b463b16c94b551632d
Manuel Hernández Padrón <37270235+ManuHP23@users.noreply.github.com>
2019-10-11 22:21:52 +0100

    ProyectoBasicoUnity

---
+++ MovCar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCar : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+
+    public float forwardForce = 1500f;
+    public float sidewaysForce = 500f;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    //Uso el método FixedUpdated() para las físicas
+    void FixedUpdate()
+    {
+        //Añadir una fuerza para avanzar
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); 
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
